8293c30534566e18de859322ab1155bc88400ff2
AdnanAhmed <127620300+AdnanAhmed04@users.noreply.github.com>
2024-02-10 17:25:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function updateTimeAndDate() {
     var a = new Date();
     var arr = a.toString().split(" ");
-
+// console.log(arr)
     var day = document.getElementById('day').innerHTML = arr[0];
+// console.log(day)
 
     let date = arr[2] + '-' + arr[1] + '-' + arr[3];
     document.getElementById('date').innerHTML = date;
+console.log(date)
 
     let time = arr[4];
     document.getElementById('time').innerHTML = time;
+    console.log(time)
+
 }
 
 setInterval(updateTimeAndDate, 1000);
